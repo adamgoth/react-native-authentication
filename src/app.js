26837764d88bf214b7aa8af8e7fab1e7500d04a4
Adam Goth <adamgoth@gmail.com>
2017-04-13 18:27:27 -0600

    Configures Firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCVIbHna7Z0WtR_vLnkECeODJJjDUEsRrU',
+      authDomain: 'react-native-auth-51c25.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-51c25.firebaseio.com',
+      projectId: 'react-native-auth-51c25',
+      storageBucket: 'react-native-auth-51c25.appspot.com',
+      messagingSenderId: '374463375287'
+    });
+  }
+
   render() {
     return (
       <View>
